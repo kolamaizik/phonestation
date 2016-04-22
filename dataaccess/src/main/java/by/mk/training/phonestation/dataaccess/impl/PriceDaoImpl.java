ca5455866b73bc7103a3611badf4432147554cc4
kolamaizik <kolamaizik@gmail.com>
2016-04-22 16:26:48 +0300

    no message

--- dataaccess/src/main/java/by/mk/training/phonestation/dataaccess/impl/PriceDaoImpl.java
+++ dataaccess/src/main/java/by/mk/training/phonestation/dataaccess/impl/PriceDaoImpl.java
@@ -1,7 +1,25 @@
 package by.mk.training.phonestation.dataaccess.impl;
 
+import java.sql.SQLException;
+
 import by.mk.training.phonestation.dataaccess.PriceDao;
+import by.mk.training.phonestation.datamodel.Price;
 
 public class PriceDaoImpl extends AbstractDaoImpl implements PriceDao {
+
+	@Override
+	public void addPrice(Price price) throws SQLException {
+		
+	}
+
+	@Override
+	public void updPrice(Price price) throws SQLException {
+
+	}
+
+	@Override
+	public Price getPriceById(Long bus_id) throws SQLException {
+		return null;
+	}
 
 }
