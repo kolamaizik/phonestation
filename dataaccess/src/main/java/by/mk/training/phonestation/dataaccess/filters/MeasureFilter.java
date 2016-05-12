c5718e2c357d539736145aacf841231071918d2d
Kola Maizik <kolamaizik@gmail.com>
2016-05-12 22:43:35 +0300

    no message

--- dataaccess/src/main/java/by/mk/training/phonestation/dataaccess/filters/MeasureFilter.java
+++ dataaccess/src/main/java/by/mk/training/phonestation/dataaccess/filters/MeasureFilter.java
@@ -1,5 +1,22 @@
 package by.mk.training.phonestation.dataaccess.filters;
 
 public class MeasureFilter extends AbstractFilter {
+	private String name;
+	private Double value;
 
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public Double getValue() {
+		return value;
+	}
+
+	public void setValue(Double value) {
+		this.value = value;
+	}
 }
---
+++ dataaccess/src/main/java/by/mk/training/phonestation/dataaccess/filters/ServiceFilter.java
@@ -0,0 +1,13 @@
+package by.mk.training.phonestation.dataaccess.filters;
+
+public class ServiceFilter extends AbstractFilter {
+	private String name;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+}
